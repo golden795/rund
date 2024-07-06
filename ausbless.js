2e91134d9ef955adaa9e3cb2f1d014f802f2d69c
golden795 <joshuaaugustine398@gmail.com>
2024-07-06 07:15:22 -0700

    Add files via upload

---
+++ ausbless.js
@@ -0,0 +1,11 @@
+function toggleNavbar() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === 'flex') {
+      navbar.style.display = 'none';
+    } else {
+      navbar.style.display = 'flex';
+    }
+  }
+
+  
+    
